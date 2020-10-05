4109f9bb62fa11cd099f58ffaea377decb64f5f7
Ktyby <kutskolukes@gmail.com>
2020-10-05 19:36:16 +0300

    feature/create-events: edit setTimeout

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 const eventsArray = [];
 
-const createEvent = (date, time, eventName, eventFunction) => {
+const createEvent = (date, time, eventFunction, eventName) => {
   const currentDate = new Date();
   const eventDate = new Date(`${date}T${time}`);
 
   const delay = eventDate.getTime() - currentDate.getTime(); 
 
+  const eventTimeout = setTimeout(eventFunction, delay);
+
   eventsArray.push({
     eventDate,
     eventFunction,
-    eventName
+    eventName,
+    eventTimeout
   });
-  
-  setTimeout(() => {
-    eventFunction();
-  }, delay);
 }
-
