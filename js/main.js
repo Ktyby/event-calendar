055cc99e6d4873358e9fb50462692459fd7759b5
Ktyby <kutskolukes@gmail.com>
2020-10-05 14:59:45 +0300

    feature/create-events: add function to create events

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("dafafs");
+const eventsArray = [];
+
+const createEvent = (date, time, eventFunction) => {
+  const currentDate = new Date();
+  const eventDate = new Date(`${date}T${time}`);
+
+  const diff = eventDate.getTime() - currentDate.getTime(); 
+
+  eventsArray.push({
+    eventDate,
+    eventFunction
+  });
+  
+  setTimeout(() => {
+    eventFunction();
+  }, diff);
+}
