c32b8eb7adc41cacc54f9c953bb86892d141c78a
Ktyby <kutskolukes@gmail.com>
2020-10-06 17:11:27 +0300

    feature/show-list: add switch for showing list

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const eventsArray = [];
+let idValue = 0;
 
 const getDelay = (date, time) => {
   const currentDate = new Date();
@@ -7,28 +8,51 @@ const getDelay = (date, time) => {
   return eventDate.getTime() - currentDate.getTime();
 }
 
-const createEvent = (date, time, eventFunction, eventName) => {
-  const eventTimeout = setTimeout(eventFunction, getDelay(date, time));
+const createEvent = (date, time, eventFunction, eventName, eventType) => {
+  let eventTimeout;
+  
+  switch (eventType) {
+    case "Once":
+      eventTimeout = setTimeout(eventFunction, getDelay(date, time));
+    break;
+    case "Every day":
+      eventTimeout = setInterval(eventFunction, getDelay(date, time));
+    break;
+    case "By selected days":
+
+    break;
+  }
+
+  const eventId = idValue++;
 
   eventsArray.push({
     eventFunction,
     eventName,
-    eventTimeout
+    eventTimeout,
+    eventId,
+    eventType,
+    eventDate: date
   });
+
+  return `Event create:
+  Date: ${date}
+  Time: ${time}
+  Name: ${eventName}
+  Event number: ${eventId}`;
 }
 
-const deleteEvent = (eventName) => {
+const deleteEvent = (currentId) => {
   eventsArray.forEach((element) => {
-    if (element.eventName === eventName) {
+    if (element.eventId === currentId) {
       clearTimeout(element.eventTimeout);
       eventsArray.splice(element, 1);
     }
   });
 }
 
-const editEvent = (currentEvent, newEventName, newEventDate, newEventTime) => {
+const editEvent = (currentId, newEventName, newEventDate, newEventTime) => {
   eventsArray.forEach((element) => {
-    if (currentEvent === element.eventName) {
+    if (currentId === element.eventId) {
       clearTimeout(element.eventTimeout);
       element.eventTimeout = setTimeout(element.eventFunction, getDelay(newEventDate, newEventTime));
       element.eventName = newEventName;
@@ -36,15 +60,27 @@ const editEvent = (currentEvent, newEventName, newEventDate, newEventTime) => {
   });
 }
 
-const showListEventsFromRange = (range) => {
-  switch (range) {
-    case "День":
-
-    break;
-    case "":
-        
-    break;  
+const showListEventsFromRange = (range, date) => {
+  const showListInRange = (dayNumber) => {
+    const eventDay = new Date(date);
     
+    let eventsList;
+
+    eventsList = eventsArray.filter((element) => {
+      return new Date(element.eventDate) === new Date(eventDay.setDate(eventDay.getDate() + index));
+    });
+
+    return eventsList;
+  }
+  
+
+  switch (range) {
+    case "Day":
+      console.table(showListInRange(1));
+    break;
+    case "Week":
+      console.table(showListInRange(7));
+    break;  
         
     default:
       break;
