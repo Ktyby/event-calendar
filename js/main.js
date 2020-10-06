3ffff879128622e4c78cc6385807a647a1ac97bd
Ktyby <kutskolukes@gmail.com>
2020-10-06 18:02:19 +0300

    feature/show-list: add ability to showing events for week

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+"use strict";
+
+const NUMBER_DAY_IN_WEEK = 7;
+const ONE_DAY = 1;
 const eventsArray = [];
 let idValue = 0;
 
@@ -62,27 +66,30 @@ const editEvent = (currentId, newEventName, newEventDate, newEventTime) => {
 
 const showListEventsFromRange = (range, date) => {
   const showListInRange = (dayNumber) => {
-    const eventDay = new Date(date);
-    
     let eventsList;
 
+    const startDate = new Date(date);
+    const endDate = new Date(startDate);
+
+    range === true ? endDate.setMonth(startDate.getMonth() + 3) : endDate.setDate(startDate.getDate() + dayNumber);
+
     eventsList = eventsArray.filter((element) => {
-      return new Date(element.eventDate) === new Date(eventDay.setDate(eventDay.getDate() + index));
+      const elementDate = new Date(element.eventDate);
+      return elementDate >= startDate && elementDate <= endDate;
     });
 
     return eventsList;
   }
   
-
   switch (range) {
     case "Day":
-      console.table(showListInRange(1));
+      console.table(showListInRange(ONE_DAY));
     break;
     case "Week":
-      console.table(showListInRange(7));
-    break;  
-        
-    default:
-      break;
+      console.table(showListInRange(NUMBER_DAY_IN_WEEK));
+    break; 
+    case "Month": 
+      console.table(showListInRange(true)); 
+    break;
   }
 }
