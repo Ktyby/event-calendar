c759ae03da1e26e0968fd4773b90b0f170d9adb0
Ktyby <kutskolukes@gmail.com>
2020-10-07 10:49:45 +0300

    feature/show-list: finish showing events list

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 
 const NUMBER_DAY_IN_WEEK = 7;
 const ONE_DAY = 1;
+const NUMBER_SECONDS_IN_DAY = 86400000;
+const WEEK_DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const eventsArray = [];
 let idValue = 0;
 
 const getDelay = (date, time) => {
   const currentDate = new Date();
   const eventDate = new Date(`${date}T${time}`);
-
+  console.log(eventDate);
   return eventDate.getTime() - currentDate.getTime();
 }
 
 const createEvent = (date, time, eventFunction, eventName, eventType) => {
   let eventTimeout;
-  
+
   switch (eventType) {
     case "Once":
       eventTimeout = setTimeout(eventFunction, getDelay(date, time));
@@ -64,14 +66,14 @@ const editEvent = (currentId, newEventName, newEventDate, newEventTime) => {
   });
 }
 
-const showListEventsFromRange = (range, date) => {
+const showListEventsFromRange = (range, date, interval) => {
   const showListInRange = (dayNumber) => {
     let eventsList;
 
     const startDate = new Date(date);
     const endDate = new Date(startDate);
 
-    range === true ? endDate.setMonth(startDate.getMonth() + 3) : endDate.setDate(startDate.getDate() + dayNumber);
+    range === "Month" ? endDate.setMonth(startDate.getMonth() + 1) : endDate.setDate(startDate.getDate() + dayNumber);
 
     eventsList = eventsArray.filter((element) => {
       const elementDate = new Date(element.eventDate);
@@ -89,7 +91,10 @@ const showListEventsFromRange = (range, date) => {
       console.table(showListInRange(NUMBER_DAY_IN_WEEK));
     break; 
     case "Month": 
-      console.table(showListInRange(true)); 
+      console.table(showListInRange("Month")); 
+    break;
+    case "Specified interval": 
+      console.table(showListInRange(interval)); 
     break;
   }
 }
